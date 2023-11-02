596c4640311f028c53f9107e5210842ac6097f0c
imrul <mohammadhasan.imrul@gmail.com>
2023-11-02 18:13:53 +0600

    dnd functionality work in progress

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,36 +1,3 @@
-// import React, { useState } from "react";
-// import style from "../../src/styles/Gallery.module.css";
-
-// const Gallery = () => {
-//   const [images, setImages] = useState([
-//       "/assets/img3.jpg",
-//       "/assets/img4.jpg",
-//       "/assets/img12.jpg",
-//     "/assets/img1.jpg",
-//     "/assets/img2.jpg",
-//     "/assets/img5.jpg",
-//     "/assets/img6.jpg",
-//     "/assets/img7.jpg",
-//     "/assets/img8.jpg",
-//     "/assets/img9.jpg",
-//     "/assets/img10.jpg",
-//     "/assets/img11.jpg",
-//     "/assets/img13.jpg",
-//   ]);
-
-//   return (
-
-//     <div className={style.galleryContainer}>
-//       <div className={style.gallery}>
-//         {images.map((img, i) => (
-//           <img key={i} src={img} alt="" />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Gallery;
 import React, { useState } from "react";
 import style from "../../src/styles/Gallery.module.css";
 
@@ -60,6 +27,7 @@ const Gallery = () => {
   ]);
 
   const [selectedImages, setSelectedImages] = useState([]);
+  
 
   const toggleImageSelection = (index) => {
     const isSelected = selectedImages.includes(index);
