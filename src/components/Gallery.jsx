6f121a327752bf88c44fa77145322a5a80f6ae1d
imrul <mohammadhasan.imrul@gmail.com>
2023-11-02 18:22:47 +0600

    grid size and image changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,25 +3,20 @@ import style from "../../src/styles/Gallery.module.css";
 
 const Gallery = () => {
   const [images, setImages] = useState([
-    "/assets/img2.jpg",
-    "/assets/img1.jpg",
-    "/assets/img3.jpg",
-    "/assets/img4.jpg",
     "/assets/img5.jpg",
+    "/assets/img4.jpg",
+    "/assets/img3.jpg",
     "/assets/img6.jpg",
     "/assets/img7.jpg",
     "/assets/img8.jpg",
     "/assets/img9.jpg",
     "/assets/img10.jpg",
     "/assets/img11.jpg",
+    "/assets/img2.jpg",
     "/assets/img12.jpg",
     "/assets/img13.jpg",
-    "/assets/img14.jpg",
-    "/assets/img15.jpg",
     "/assets/img16.jpg",
-    "/assets/img17.jpg",
     "/assets/img18.jpg",
-    "/assets/img19.jpg",
     "/assets/img20.jpg",
     "/assets/img21.jpg",
   ]);
