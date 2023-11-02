d50baed62aa9d17b4a6e18166cbde234bb110026
imrul <mohammadhasan.imrul@gmail.com>
2023-11-02 12:55:03 +0600

    image select and delete functionality added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,15 +1,46 @@
+// import React, { useState } from "react";
+// import style from "../../src/styles/Gallery.module.css";
+
+// const Gallery = () => {
+//   const [images, setImages] = useState([
+//       "/assets/img3.jpg",
+//       "/assets/img4.jpg",
+//       "/assets/img12.jpg",
+//     "/assets/img1.jpg",
+//     "/assets/img2.jpg",
+//     "/assets/img5.jpg",
+//     "/assets/img6.jpg",
+//     "/assets/img7.jpg",
+//     "/assets/img8.jpg",
+//     "/assets/img9.jpg",
+//     "/assets/img10.jpg",
+//     "/assets/img11.jpg",
+//     "/assets/img13.jpg",
+//   ]);
+
+//   return (
+
+//     <div className={style.galleryContainer}>
+//       <div className={style.gallery}>
+//         {images.map((img, i) => (
+//           <img key={i} src={img} alt="" />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Gallery;
 import React, { useState } from "react";
 import style from "../../src/styles/Gallery.module.css";
 
-
-
-
 const Gallery = () => {
   const [images, setImages] = useState([
-      "/assets/img3.jpg",
+    "/assets/img3.jpg",
+    "/assets/img4.jpg",
+    "/assets/img12.jpg",
     "/assets/img1.jpg",
     "/assets/img2.jpg",
-    "/assets/img4.jpg",
     "/assets/img5.jpg",
     "/assets/img6.jpg",
     "/assets/img7.jpg",
@@ -17,19 +48,44 @@ const Gallery = () => {
     "/assets/img9.jpg",
     "/assets/img10.jpg",
     "/assets/img11.jpg",
-    "/assets/img12.jpg",
     "/assets/img13.jpg",
   ]);
 
+  const [selectedImages, setSelectedImages] = useState([]);
 
-  
+  const toggleImageSelection = (index) => {
+    const isSelected = selectedImages.includes(index);
+    if (isSelected) {
+      setSelectedImages(selectedImages.filter((i) => i !== index));
+    } else {
+      setSelectedImages([...selectedImages, index]);
+    }
+  };
+
+  const deleteSelectedImages = () => {
+    const updatedImages = images.filter(
+      (_, index) => !selectedImages.includes(index)
+    );
+    setImages(updatedImages);
+    setSelectedImages([]);
+  };
+
   return (
-   
-    
     <div className={style.galleryContainer}>
+      {selectedImages.length > 0 && (
+        <div className={style.deleteBtn}>
+          <img src="/assets/trash.gif" onClick={deleteSelectedImages} alt="" />
+          <p>Click the Bin to Delete</p>
+        </div>
+      )}
       <div className={style.gallery}>
         {images.map((img, i) => (
-          <img key={i} src={img} alt="" />
+          <img
+            src={img}
+            alt=""
+            className={`${selectedImages.includes(i) ? style.selected : ""}`}
+            onClick={() => toggleImageSelection(i)}
+          />
         ))}
       </div>
     </div>
